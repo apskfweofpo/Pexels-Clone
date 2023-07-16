09a531d3cdd45ed965d349c203a8078e8b381c71
Pavel Krikunov <fusrodah.df@mail.ru>
2023-07-16 19:18:10 +0300

    add Loader

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styles from "./Loader.module.scss";
+
+export const Loader = () => {
+  return (
+    <div className={styles.ldsRing}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
