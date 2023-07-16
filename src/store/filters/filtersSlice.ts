1941bbbaaed9cb17c8a64c7e61b2a18f28d9b6cf
Pavel Krikunov <fusrodah.df@mail.ru>
2023-07-16 19:34:54 +0300

    add color, size and orientation

--- src/store/filters/filtersSlice.ts
+++ src/store/filters/filtersSlice.ts
@@ -1,13 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../store";
-import { ICategory } from "../../utils/interfaces";
+import { ICategory, Orientations, Sizes } from "../../utils/interfaces";
 
 // Define a type for the slice state
 interface IFilterState {
   page: number;
   perPage: number;
   category: ICategory;
+  search: string;
+  color: string;
+  size: string;
+  orientation: string;
 }
 
 // Define the initial state using that type
@@ -15,6 +19,10 @@ const initialState: IFilterState = {
   page: 1,
   perPage: 12,
   category: { meta: "", name: "" },
+  search: "",
+  color: "",
+  size: "",
+  orientation: "",
 };
 
 export const filtersSlice = createSlice({
@@ -31,10 +39,37 @@ export const filtersSlice = createSlice({
     setCategory: (state, action: PayloadAction<ICategory>) => {
       state.category = action.payload;
     },
+    setSearch: (state, action: PayloadAction<string>) => {
+      state.search = action.payload;
+    },
+
+    setOrientation: (state, action: PayloadAction<Orientations>) => {
+      state.orientation = action.payload;
+    },
+    setColor: (state, action: PayloadAction<string>) => {
+      state.color = action.payload;
+    },
+    setSize: (state, action: PayloadAction<Sizes>) => {
+      state.size = action.payload;
+    },
+    clearFilters: (state) => {
+      state.size = "";
+      state.color = "";
+      state.orientation = "";
+    },
   },
 });
 
-export const { nextPage, prevPage, setCategory } = filtersSlice.actions;
+export const {
+  nextPage,
+  prevPage,
+  setCategory,
+  setSearch,
+  setOrientation,
+  setColor,
+  setSize,
+  clearFilters,
+} = filtersSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectPage = (state: RootState) => state.filters.page;
